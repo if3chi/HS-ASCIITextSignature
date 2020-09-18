dea85ac0721b35b632950d93182de46233020dbb
ifechi <j.cyxtus@gmail.com>
2020-09-18 03:36:20 +0000

    Tried Using .map in the x range.

--- Problems/The roots of equation/src/Task.kt
+++ Problems/The roots of equation/src/Task.kt
@@ -1,8 +1,8 @@
 import java.util.*
 import kotlin.math.pow
 
-fun checkRoot(a: Int, b: Int, c: Int, d: Int, i: Int): Boolean = a * i.toDouble().pow(3.0) +
-        b * i.toDouble().pow(2.0) + c * i + d == 0.toDouble()
+fun checkRoot(a: Int, b: Int, c: Int, d: Int, i: Double): Boolean = a * i.pow(3.0) +
+        b * i.pow(2.0) + c * i + d == 0.toDouble()
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
 
     val (a, b, c, d) = IntArray(4) { scanner.nextInt() }
 
-    for (i in 0..1000) {
-        if (checkRoot(a, b, c, d, i)) println(i)
+    for (x in (0..1000).map {it.toDouble()}) {
+        if (checkRoot(a, b, c, d, x)) println(x.toInt())
     }
 }
