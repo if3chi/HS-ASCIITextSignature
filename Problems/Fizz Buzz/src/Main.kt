d37322003d369a4d16bfbd21ab5fe76ef4746f22
ifechi <j.cyxtus@gmail.com>
2020-09-18 13:06:18 +0000

    Solved PoD: Fizz Buzz.

--- Problems/Fizz Buzz/src/Main.kt
+++ Problems/Fizz Buzz/src/Main.kt
@@ -1,7 +1,24 @@
 import java.util.*
 
+fun usingIf(n: Int) {
+    if (n % 3 == 0 && n % 5 == 0) {
+            println("FizzBuzz")
+        } else if (n % 3 == 0) {
+            println("Fizz")
+        } else if (n % 5 == 0) {
+            println("Buzz")
+        } else {
+            println(n)
+        }
+}
+
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     // write your code here
+    val (a, b) = IntArray(2) { scanner.nextInt() }
+
+    for (n in a..b) {
+        usingIf(n)
+    }
 }
