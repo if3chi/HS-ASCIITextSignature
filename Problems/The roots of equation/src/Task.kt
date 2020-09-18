5358723aff01757781a88f844cdd2c95044158c8
ifechi <j.cyxtus@gmail.com>
2020-09-18 03:24:52 +0000

    Solved PoD: The roots of the equation.

--- Problems/The roots of equation/src/Task.kt
+++ Problems/The roots of equation/src/Task.kt
@@ -1,6 +1,16 @@
 import java.util.*
+import kotlin.math.pow
+
+fun checkRoot(a: Int, b: Int, c: Int, d: Int, i: Int): Boolean = a * i.toDouble().pow(3.0) +
+        b * i.toDouble().pow(2.0) + c * i + d == 0.toDouble()
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     // put your code here
+
+    val (a, b, c, d) = IntArray(4) { scanner.nextInt() }
+
+    for (i in 0..1000) {
+        if (checkRoot(a, b, c, d, i)) println(i)
+    }
 }
