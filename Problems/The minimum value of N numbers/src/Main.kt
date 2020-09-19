c61efed7c4224330b4f21ed7df831293d76a1f90
ifechi <j.cyxtus@gmail.com>
2020-09-19 10:26:41 +0000

    Solved PoD Method 2: The minimum value of N numbers.

--- Problems/The minimum value of N numbers/src/Main.kt
+++ Problems/The minimum value of N numbers/src/Main.kt
@@ -4,18 +4,21 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     // write your code here
-    val numN = scanner.nextInt()
-    var min = 1
+//    val numN = scanner.nextInt()
+//    var min = 1
+//
+//    for (n in 1..numN) {
+//        val num = scanner.nextInt()
+//        if (n == 1) {
+//            min = num
+//        }
+//        if (num <= min) {
+//            min = num
+//        }
+//    }
 
-    for (n in 1..numN) {
-        val num = scanner.nextInt()
-        if (n == 1) {
-            min = num
-        }
-        if (num <= min) {
-            min = num
-        }
-    }
+    val arr = IntArray(scanner.nextInt()) { scanner.nextInt() }
+    print(arr.min())
 
-    println(min)
+//    println(min)
 }
