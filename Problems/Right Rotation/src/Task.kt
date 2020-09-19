6ca62767bfdd748ff1b268b4fcf569945be2d1b5
ifechi <j.cyxtus@gmail.com>
2020-09-19 11:03:47 +0000

    Solved Right Rotation problem.

--- Problems/Right Rotation/src/Task.kt
+++ Problems/Right Rotation/src/Task.kt
@@ -4,4 +4,8 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     // write your code here
+    val arr = IntArray(scanner.nextInt()) { scanner.nextInt() }
+    val list = arr.toList()
+    Collections.rotate(list, scanner.nextInt())
+    println(list.toIntArray().joinToString(" "))
 }
