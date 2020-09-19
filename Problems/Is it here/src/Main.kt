8d33aae3d79d562f4754eb84c796cc84f2a23f17
ifechi <j.cyxtus@gmail.com>
2020-09-19 11:23:12 +0000

    Solved Problem: Is it here?

--- Problems/Is it here/src/Main.kt
+++ Problems/Is it here/src/Main.kt
@@ -4,4 +4,17 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     // write your code here
+    val arr = IntArray(scanner.nextInt()) { scanner.nextInt() }
+    val m = scanner.nextInt()
+//    var found = false
+//
+//    for (n in arr) {
+//        if (n == m) {
+//            found = true
+//            break
+//        }
+//    }
+//
+//    print(if (found) "YES" else "NO")
+    print(if (m in arr) "YES" else "NO")
 }
