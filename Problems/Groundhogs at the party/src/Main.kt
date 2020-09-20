167a5cf35ab4840ac58a9a5c5b2fb6a3c323d23c
ifechi <j.cyxtus@gmail.com>
2020-09-20 14:03:26 +0000

    Solved PoD: Groundhogs at the party

--- Problems/Groundhogs at the party/src/Main.kt
+++ Problems/Groundhogs at the party/src/Main.kt
@@ -3,4 +3,13 @@ import java.util.*
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     // write your code here
+
+    val cups = scanner.nextInt()
+    val weekType = scanner.nextBoolean()
+
+    if (weekType) {
+        print(cups in 15..25)
+    } else {
+        print(cups in 10..20)
+    }
 }
