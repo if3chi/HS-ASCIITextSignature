fb497c014737acfb72c00540c3b4998637a93a2f
ifechi <j.cyxtus@gmail.com>
2020-09-21 16:46:29 +0000

    Solved Task: Balance checker.

--- Problems/Balance checker/src/Main.kt
+++ Problems/Balance checker/src/Main.kt
@@ -3,5 +3,29 @@ import java.util.*
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
-    // write your code here
+    var balance = scanner.nextInt()
+    val p = mutableListOf<Int>()
+
+    while (scanner.hasNext()) {
+        p.add(scanner.nextInt())
+    }
+
+    for (i in 0..p.size) {
+        if (balance >= p[i]) {
+            balance -= p[i]
+            if (balance == 0 && i == p.lastIndex) {
+                print("Thank you for choosing us to manage your account! " +
+                        "You have 0 money.")
+                break
+            } else if (i == p.lastIndex && balance != 0) {
+                print("Thank you for choosing us to manage your account! " +
+                        "You have $balance money.")
+                break
+            }
+        } else {
+            print("Error, insufficient funds for the purchase. " +
+                    "You have $balance, but you need ${p[i]}.")
+            break
+        }
+    }
 }
