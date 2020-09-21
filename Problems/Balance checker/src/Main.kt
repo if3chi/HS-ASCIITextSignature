ed8ceffe27be639b6ba493f7b97d30d2d8c1672a
ifechi <j.cyxtus@gmail.com>
2020-09-21 17:00:03 +0000

    Solved Task: Balance checker. Method 2

--- Problems/Balance checker/src/Main.kt
+++ Problems/Balance checker/src/Main.kt
@@ -4,28 +4,23 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     var balance = scanner.nextInt()
-    val p = mutableListOf<Int>()
+    var enough = true
+    var purchase = 0
 
     while (scanner.hasNext()) {
-        p.add(scanner.nextInt())
-    }
+        purchase = scanner.nextInt()
 
-    for (i in 0..p.size) {
-        if (balance >= p[i]) {
-            balance -= p[i]
-            if (balance == 0 && i == p.lastIndex) {
-                print("Thank you for choosing us to manage your account! " +
-                        "You have 0 money.")
-                break
-            } else if (i == p.lastIndex && balance != 0) {
-                print("Thank you for choosing us to manage your account! " +
-                        "You have $balance money.")
-                break
-            }
+        if (balance >= purchase) {
+            balance -= purchase
         } else {
-            print("Error, insufficient funds for the purchase. " +
-                    "You have $balance, but you need ${p[i]}.")
+            enough = false
             break
         }
+    }
+
+    if (enough) {
+        println("Thank you for choosing us to manage your account! You have $balance money.")
+    } else {
+        print("Error, insufficient funds for the purchase. You have $balance, but you need $purchase.")
     }
 }
