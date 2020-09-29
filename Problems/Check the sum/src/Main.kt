e27981b70fabe17d6e070440590c95ce764e2cf8
ifechi <j.cyxtus@gmail.com>
2020-09-29 01:01:05 +0000

    Solved PoD: Check the sum.

--- Problems/Check the sum/src/Main.kt
+++ Problems/Check the sum/src/Main.kt
@@ -1,13 +1,9 @@
 import java.util.*
 
-// write your code here
-fun isRightEquation(a, b, c) = a * b == c
-
-/* Do not change code below */
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val a = scanner.nextInt()
     val b = scanner.nextInt()
     val c = scanner.nextInt()
-    println(isRightEquation(a, b, c))
+    println(a + b == 20 || a + c == 20 || b + c == 20)
 }
