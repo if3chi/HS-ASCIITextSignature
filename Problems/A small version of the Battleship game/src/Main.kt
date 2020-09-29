0895f97b9ca0c12199181474e8e595c94dbeeced
ifechi <j.cyxtus@gmail.com>
2020-09-29 00:49:46 +0000

    Solving PoD: Check the sum.

--- Problems/A small version of the Battleship game/src/Main.kt
+++ Problems/A small version of the Battleship game/src/Main.kt
@@ -3,4 +3,10 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     // put your code here
+
+    val coor = mutableListOf<Int>()
+
+    while (input.hasNext()) coor.add(input.nextInt())
+
+    print(coor)
 }
